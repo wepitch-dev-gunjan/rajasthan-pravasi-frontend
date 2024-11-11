9b878f9895258c88b1e54245e68a681aeb9f93b1
Sandeepbisht1208 <sandeepbisht1208@gmail.com>
2024-11-11 17:30:47 +0530

    responive 3

--- src/pages/home/imageGallery/index.jsx
+++ src/pages/home/imageGallery/index.jsx
@@ -1,7 +1,5 @@
-
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 
 // Import Swiper styles
 import 'swiper/css';
@@ -10,7 +8,7 @@ import 'swiper/css/pagination';
 
 import './style.scss';
 
-// import required modules
+// Import required modules
 import { EffectCoverflow, Pagination } from 'swiper/modules';
 
 const ImageGallery = () => {
@@ -18,58 +16,63 @@ const ImageGallery = () => {
     <section className="gallery-section">
       <h2>Gallery</h2>
       <div className="gallerySection">
-      <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        loop={true}
-        centeredSlides={true}
-        slidesPerView={'4'}
-        coverflowEffect={{
-          rotate: 0,
-        stretch: 80,
-        depth: 200,
-        modifier: 1,
-        slideShadows: true,
-        }}
-        pagination={false}
-        modules={[EffectCoverflow, Pagination]}
-        
-        className="mySwiper"
-      >
-        <SwiperSlide className='swiperSlider'>
-          <img alt='test' src="https://swiperjs.com/demos/images/nature-1.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img alt='test' src="https://swiperjs.com/demos/images/nature-2.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img alt='test' src="https://swiperjs.com/demos/images/nature-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img alt='test' src="https://swiperjs.com/demos/images/nature-4.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img alt='test' src="https://swiperjs.com/demos/images/nature-5.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img alt='test' src="https://swiperjs.com/demos/images/nature-6.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img alt='test' src="https://swiperjs.com/demos/images/nature-7.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img alt='test' src="https://swiperjs.com/demos/images/nature-9.jpg" />
-        </SwiperSlide>
-      </Swiper>
-
-
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          loop={true}
+          centeredSlides={true}
+          slidesPerView={4} // Default for larger screens
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 80,
+            depth: 200,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={false}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper"
+          breakpoints={{
+            640: { // 640px and up
+              slidesPerView: 4,
+            },
+            0: { // Below 640px
+              slidesPerView: 1,
+            },
+          }}
+        >
+          <SwiperSlide className='swiperSlider'>
+            <img alt='test' src="https://swiperjs.com/demos/images/nature-1.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img alt='test' src="https://swiperjs.com/demos/images/nature-2.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img alt='test' src="https://swiperjs.com/demos/images/nature-3.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img alt='test' src="https://swiperjs.com/demos/images/nature-4.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img alt='test' src="https://swiperjs.com/demos/images/nature-5.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img alt='test' src="https://swiperjs.com/demos/images/nature-6.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img alt='test' src="https://swiperjs.com/demos/images/nature-7.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img alt='test' src="https://swiperjs.com/demos/images/nature-8.jpg" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img alt='test' src="https://swiperjs.com/demos/images/nature-9.jpg" />
+          </SwiperSlide>
+        </Swiper>
       </div>
       <button className="view-all-btn">View All</button>
     </section>
-  )
+  );
 }
 
-export default ImageGallery
+export default ImageGallery;
